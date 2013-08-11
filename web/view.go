7a5f13433c32746b882e4d46e4ba4750ad1a7cd5
Tomek Szpinda <tomasz.szpinda@oneserve.co.uk>
2013-08-11 16:53:41 +0100

    path fix

--- web/view.go
+++ web/view.go
@@ -8,15 +8,15 @@ import (
 )
 
 func Mount() {
-	pfx := "/static/img/"
-	h := http.StripPrefix(pfx, http.FileServer(http.Dir("./static/img")))
+	pfx := "/public/static/img/"
+	h := http.StripPrefix(pfx, http.FileServer(http.Dir("./public/static/img")))
 	http.Handle(pfx, h)
 	
-	serveSingle("/static/js/main.js", "./static/js/main.js")
- 	serveSingle("/static/js/jquery.js", "./static/js/jquery.js")
- 	serveSingle("/static/bootstrap/js/bootstrap.js", "./static/bootstrap/js/bootstrap.js")
- 	serveSingle("/static/bootstrap/css/bootstrap.css", "./static/bootstrap/css/bootstrap.min.css")
- 	serveSingle("/static/bootstrap/css/bootstrap-responsive.css", "./static/bootstrap/css/bootstrap-responsive.min.css")
+	serveSingle("/static/js/main.js", "./public/static/js/main.js")
+ 	serveSingle("/static/js/jquery.js", "./public/static/js/jquery.js")
+ 	serveSingle("/static/bootstrap/js/bootstrap.js", "./public/static/bootstrap/js/bootstrap.js")
+ 	serveSingle("/static/bootstrap/css/bootstrap.css", "./public/static/bootstrap/css/bootstrap.min.css")
+ 	serveSingle("/static/bootstrap/css/bootstrap-responsive.css", "./public/static/bootstrap/css/bootstrap-responsive.min.css")
 	http.HandleFunc("/index", index)
 }
 
@@ -28,7 +28,7 @@ func serveSingle(pattern string, filename string) {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		var templateName = "tmpl/search.gtpl"
+		var templateName = "public/tmpl/search.gtpl"
 		t, err := template.ParseFiles(templateName)
 		if err != nil {
 			log.Fatal("unable to parse", templateName, err)
